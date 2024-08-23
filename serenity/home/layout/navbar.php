686c100e39107268e6681a75c7942676afed4945
Lijala Tuladhar <lijalaa@gmail.com>
2024-08-23 16:05:56 +0545

    Account settings update

--- serenity/home/layout/navbar.php
+++ serenity/home/layout/navbar.php
@@ -26,12 +26,12 @@ $img=imgPath;
         <a href="../../home/pages/homepg.php"
          class="a <?php echo isset($page) && $page === 'Home' ? 'active' : ''; ?>" >Home </a>
         <a href="../../home/pages/aboutUs.php"
-        class=" a<?php echo isset($page) && $page === 'About Us' ? 'active' : ''; ?>">About Us</a>
+        class=" a <?php echo isset($page) && $page === 'About Us' ? 'active' : ''; ?>">About Us</a>
         <a href="../../home/pages/products.php"
         class="a <?php echo isset($page) && $page === 'Store' ? 'active' : ''; ?>">Products</a>
         <a href="" class="a">Contact Us</a>
         <a href="../../home/pages/form.php" class="a <?php echo isset($page) && $page === 'Register' ? 'active' : ''; ?>">Login </a>
-        <a href=""><i class="a fa fa-question-circle fa-lg" aria-hidden="true"></i></a>
+        <a href="../../home/pages/admin.php"><i class="a fa fa-question-circle fa-lg" aria-hidden="true"></i></a>
         <a href="../../home/pages/appointment.php" class="">
         <button class="primary" href>Appointment</button>
         </a>
