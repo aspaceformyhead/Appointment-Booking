39d665de4b4f1ef4541cce5ef8e862c54bf41b99
Lijala Tuladhar <lijalaa@gmail.com>
2024-09-04 14:36:36 +0545

    Folder separation

---
+++ appointment/pages/signup.php
@@ -0,0 +1,38 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/signup.css">
+    <title>Document</title>
+</head>
+<body>
+<h2>WELCOME!</h2>
+
+    <form action="">
+
+        <label for="firstName" > First name</label>
+        <label for="LastName"> Last Name</label>
+
+        <input type="text" placeholder=" First Name">
+        <input type="text" placeholder=" Last Name">
+
+        <label for="email"> Email</label>
+        <label for="Phone"> Phone</label>
+
+        <input type="email" placeholder=" eg: abc@gmail.com">
+        <input type="text" placeholder=" Phone number">
+
+        <label for="password"> Password</label>
+        <label for="LastName"> Confirm Password</label>
+
+        <input type="password" placeholder="password">
+        <input type="password" placeholder="Enter the password again">
+
+        <button class="primary">Sign Up</button>
+        <button>Login</button>
+    </form>
+    
+</body>
+</html>
