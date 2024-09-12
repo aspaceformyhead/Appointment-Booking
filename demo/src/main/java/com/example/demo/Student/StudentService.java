3e95e40fbe199ef6bfb1a7f0ae5b436ea05e744c
Lijala Tuladhar <lijalaa@gmail.com>
2024-09-12 17:16:20 +0545

    Data Access layer

--- demo/src/main/java/com/example/demo/Student/StudentService.java
+++ demo/src/main/java/com/example/demo/Student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.Student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,17 +10,17 @@ import java.util.List;
 
 @Service
 public class StudentService {
-    public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "mariam@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY,20),
-                        21
+    private final StudentRepository studentRepository;
 
-                )
-        );
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Autowired
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+
     }
 
 
