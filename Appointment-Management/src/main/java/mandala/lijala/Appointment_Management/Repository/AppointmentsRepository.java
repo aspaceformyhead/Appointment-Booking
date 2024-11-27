e5ec996ff3afdbf8e2988b1d5263272c3ce94474
Manish <manishmalla128@gmail.com>
2024-11-27 13:30:32 +0545

    appointment changes made

--- Appointment-Management/src/main/java/mandala/lijala/Appointment_Management/Repository/AppointmentsRepository.java
+++ Appointment-Management/src/main/java/mandala/lijala/Appointment_Management/Repository/AppointmentsRepository.java
@@ -2,8 +2,10 @@ package mandala.lijala.Appointment_Management.Repository;
 
 import mandala.lijala.Appointment_Management.Model.Appointments;
 import mandala.lijala.Appointment_Management.Model.Doctor;
+import mandala.lijala.Appointment_Management.Model.Organization;
 import mandala.lijala.Appointment_Management.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -12,6 +14,8 @@ import java.util.List;
 public interface AppointmentsRepository extends JpaRepository<Appointments, Integer> {
     List<Appointments> findByDoctor(Doctor doctor);
     List<Appointments> findByUserID(User userID);
+    @Query("SELECT a FROM Appointments a WHERE a.display = true")
+    List<Appointments> findByOrganization(Organization organization);
     boolean existsByDoctorAndAppDateAndAppTime(Doctor doctor, LocalDate appDate, LocalTime appTime);
 
 
